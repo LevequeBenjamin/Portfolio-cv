c45a18a68117e48fd3b22d5bf34c364a2a10730c
bosso74 <bosso27@live.fr>
2021-02-23 16:11:56 +0100

    first commit

---
+++ portfolio-cv/src/data/portfolioData.js
@@ -0,0 +1,47 @@
+export const portfolioData = [
+  {
+    id: 1,
+    name: 'Reservia',
+    languages: ['css'],
+    languagesIcons: ['fab fa-css3-alt'],
+    source: 'https://github.com/LevequeBenjamin/BenjaminLeveque_2_01122020',
+    info: "Le sujet du projet 2 du parcours Développeur web chez Openclassrooms porte sur la création d'une première version du site permettant aux usagers de trouver des hébergements et des activités dans la ville de leur choix.",
+    picture: './media/project1.png'
+  },
+  {
+    id: 2,
+    name: 'Oh My Food',
+    languages: ['css'],
+    languagesIcons: ['fab fa-css3-alt'],
+    source: 'https://github.com/LevequeBenjamin/BenjaminLeveque_3_13122020',
+    info: "Le sujet du projet 3 du parcours Développeur web OpenClassrooms porte sur la création d'une première version du site 100% mobile qui répertorie les menus de restaurants gastronomiques. En plus des systèmes classiques de réservation, les clients pourront composer le menu de leur repas pour que les plats soient prêts à leur arrivée.",
+    picture: './media/project2.png'
+  },
+  {
+    id: 3,
+    name: 'La Chouette Agence',
+    languages: ['javascript','css'],
+    languagesIcons: ['fab fa-js','fab fa-css3-alt'],
+    source: 'https://github.com/LevequeBenjamin/BenjaminLeveque_4_28122020',
+    info: "Le sujet porte sur l'optimisation du site La Chouette agence, (hébergé sur GitHub) via la mise en œuvre de 10 recommandations SEO. Il faut également garantir l'accessibilité mobile du site en respectant les critères d'accessibilité sur WCAG v2.0.",
+    picture: './media/project3.png'
+  },
+  {
+    id: 4,
+    name: 'Oricamera',
+    languages: ['javascript', 'css'],
+    languagesIcons: ['fab fa-js', 'fab fa-css3-alt'],
+    source: 'https://github.com/LevequeBenjamin/BenjaminLeveque_P5_15012021',
+    info: "Le sujet du projet 5 du parcours Développeur web chez Openclassrooms porte sur la création d’un site e- commerce pour la partie front-end.L'objectif étant de créer un premier MVP pour démontrer le fonctionnement des applications à des investisseurs potentiels.",
+    picture: './media/project4.png'
+  },
+  {
+    id: 5,
+    name: 'Piquante',
+    languages: ['javascript','css'],
+    languagesIcons: ['fab fa-js', 'fab fa-css3-alt'],
+    source: 'https://github.com/LevequeBenjamin/BenjaminLeveque_P6_08022021',
+    info: "Le sujet du projet 6 du parcours Développeur web chez Openclassrooms porte sur le développement d'une application d’évaluation des sauces piquantes pour la marque 'So Pekocko'. L'objectif étant de créer un MVP permettant aux utilisateurs d’ajouter leurs sauces préférées et de liker ou disliker les sauces ajoutées par les autres utilisateurs.",
+    picture: './media/project5.png'
+  },
+]
