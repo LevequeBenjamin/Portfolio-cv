f35a1975c51c658fac5911bf2a6d4b1737e40d66
Bosso <bosso27@live.fr>
2021-02-23 22:44:13 +0100

    ajout photos

---
+++ portfolio-cv/src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+
+const Home = () => {
+  return (
+    <div>
+      <Navigation />
+      <h3>Ici c'est home</h3>
+    </div>
+  );
+};
+
+export default Home;
---
+++ portfolio-cv/src/pages/Knowledges.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+
+const Knowledges = () => {
+  return (
+    <div>
+      <Navigation />
+      <h3>Ici c'est knowledges</h3>
+    </div>
+  );
+};
+
+export default Knowledges;
